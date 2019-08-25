98f576a110712ff57ba566a38b2ed11c2731a72e
ns-spitsyna <spitcinan@gmail.com>
2019-08-25 15:53:36 +0700

    Домашнее задание 2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-var money;
-var income; 
-var addExpenses; 
-var deposit; 
-var mission; 
-var period;
+var money = 35000; // Доход за месяц
+var income = 'Фриланс'; // Доп доход
+var addExpenses = 'Коммуналка, проезд, связь'; //Расходы
+var deposit = true; 
+var mission = 10000000; 
+var period = 12;
+var budgetDay = (money/30).toFixed(2); //Бюджет на день
+var	balance = money%30; // Остаток от деления
 
-alert("Хочу спать!");
-console.log("Дима, пойдем спать");
+
+console.log('Типы данных: ' + ' ' + typeof money, typeof income, typeof deposit);
+console.log('Длина строки "Фриланс": ' + ' ' + income.length);
+console.log('Период:'+ ' ' + period + ' ' +'месяцев\n' 
+			+ 'Цель: заработать'+' ' + mission + ' ' + 'рублей');
+console.log('Дополнительные расходы:' + ' ' + addExpenses.toLowerCase().split(', ')); 	
+console.log('Бюджет на день:' + ' ' + budgetDay + '\n' + 'Остаток от деления:' + ' ' + balance);
+
+// alert("Хочу спать!");
+// console.log("Дима, пойдем спать");
 
 
