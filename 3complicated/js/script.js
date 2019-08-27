f51c3e7dd22ff77cf45f3bbbcf67ec0a949dae08
ns-spitsyna <spitcinan@gmail.com>
2019-08-27 23:56:35 +0700

    Усложненное задание 3

---
+++ 3complicated/js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var ru = ["пн, вт, ср, чт, пт, сб, вс"];
+var en = ["Mon, Tues, Wedn, Thur, Fri, Sat, Sun"];
+var lang = en;
+
+// if (lang = ru){
+// 	console.log(ru);
+// } else if (lang = en){
+// 	console.log(en);
+// }
+
+// switch (lang) {
+// 	case ru:
+// 		console.log(ru);
+// 		break;
+// 	case en:
+// 		console.log(en);
+// 		break;
+// 	default:
+// 		break;
+// }
+
+var lang = [ru, en];
+//console.log(lang);
+//console.log(lang[0]);
+
+//var x = (lang = en) ? console.log(lang[1]) : console.log(lang[0]);
+var namePerson = 'asdfgh';
+var namePerson = 'Артем' ? console.log('директор') : console.log('студент');
+var namePerson = 'Максим' ? console.log('преподаватель') : console.log('студент');
+
+
+
