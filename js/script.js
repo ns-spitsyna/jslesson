79df49b86fb0e6e4773292eb65214f6abe77cfd5
ns-spitsyna <spitcinan@gmail.com>
2019-09-03 20:59:07 +0700

    Усложненное задание 5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 'use strict';
-
-let data;
-function getOneArgument(data){
-	data = prompt("Введите строку:", "");
-
-	if(typeof(data) != 'string'){
-		return "Не строка!";
-	} else {
-		return data.replace(/\s+/g, '').slice(0, 29) + '...';
-	}
-	
+let arr = ["2222222", "409999", "45755", "1111111", "717171", "68879888", "4785966"];
+for (let i = 0; i < arr.length; i++){
+	(arr[i].charAt(0) == 2 || arr[i].charAt(0) == 4) ? console.log (arr[i]) : " " ;
 };
 
-console.log(getOneArgument(data));
+for (let i = 2; i < 100; i++){
+	let x = 2**i - 1;
+	console.log (x);
+	
+}
+
+
+
+
+
