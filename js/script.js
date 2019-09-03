61faef3911f278d4e0ef151a4f819ae9a4cfd6cd
ns-spitsyna <spitcinan@gmail.com>
2019-09-03 21:00:38 +0700

    Усложненное задание 5

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 'use strict';
 let arr = ["2222222", "409999", "45755", "1111111", "717171", "68879888", "4785966"];
 for (let i = 0; i < arr.length; i++){
-	(arr[i].charAt(0) == 2 || arr[i].charAt(0) == 4) ? console.log (arr[i]) : " " ;
+	(arr[i].charAt(0) == 2 || arr[i].charAt(0) == 4) ? 
+	console.log ('Числа, которые начинаются с цифры 2 или 4' + ' ' + arr[i]) : " " ;
 };
 
-for (let i = 2; i < 100; i++){
-	let x = 2**i - 1;
-	console.log (x);
+let n = 100;
+nextNumber:
+for (let i = 2; i < n; i++){
+	for (let j = 2; j < i; j++){
+		let x = i % j;
+		if (x == 0) continue nextNumber;
+
+	}
 	
+	console.log(i + ' ' + 'Делители этого числа: 1 и' + ' ' + i);
 }
 
 
